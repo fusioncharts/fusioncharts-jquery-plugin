ba76853a8b575391f3577bc7daf35927be2104e2
Meher Howji <138777111+meherhowji@users.noreply.github.com>
2026-03-04 13:44:11 +0530

    SUPPORT-2509: update pipeline, rename artifacts, stabilize tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,70 +1,56 @@
 // Karma configuration
 // Generated on Wed Mar 08 2017 15:56:23 GMT+0530 (IST)
-module.exports = function(config) {
-  config.set({
-
+// TODO: requires Node 8.17.0, to be upgraded
+module.exports = function (config) {
+    config.set({
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: './',
-
+        basePath: './',
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'browserify'],
-
+        frameworks: ['jasmine', 'browserify'],
 
     // list of files / patterns to load in the browser
-    files: [
-      'test/*.js'
-    ],
-
+        files: ['test/*.js'],
 
     // list of files to exclude
-    exclude: [
-    ],
-
+        exclude: [],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-        'test/*.js': [ 'browserify' ]
-    },
-
+        preprocessors: {
+            'test/*.js': ['browserify'],
+        },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
+        reporters: ['progress'],
 
     // web server port
-    port: 9876,
-
+        port: 9876,
 
     // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
+        colors: true,
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
+        logLevel: config.LOG_INFO,
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
+        autoWatch: true,
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     // browsers: ['Chrome', 'Firefox'],
-    browsers: ['Chrome'],
-
+        browsers: ['Chrome'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
+        singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
-}
+        concurrency: Infinity,
+    });
+};
